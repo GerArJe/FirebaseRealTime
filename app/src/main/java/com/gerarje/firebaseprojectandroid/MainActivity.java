c284256ced4213c44f0335c4d0ddaa4a8e31ce1f
German Arevalo <germanarevalojerez@gmail.com>
2019-04-25 17:10:47 -0500

    first commit + Firebase Real-Time

---
+++ app/src/main/java/com/gerarje/firebaseprojectandroid/MainActivity.java
@@ -0,0 +1,85 @@
+package com.gerarje.firebaseprojectandroid;
+
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import com.gerarje.firebaseprojectandroid.model.Artist;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final String ARTIST_NODE = "Artists";
+    private static final String TAG = "MainActivity";
+    private DatabaseReference databaseReference;
+
+    private ListView lstArtist;
+    private ArrayAdapter arrayAdapter;
+    private List<String> artistNames;
+    private List<Artist> artists;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        lstArtist = findViewById(R.id.lstArtist);
+        artistNames = new ArrayList<>();
+        artists = new ArrayList<>();
+        arrayAdapter = new ArrayAdapter(this, android.R.layout.simple_expandable_list_item_1, artistNames);
+        lstArtist.setAdapter(arrayAdapter);
+
+        FirebaseDatabase.getInstance().setPersistenceEnabled(true);
+        databaseReference = FirebaseDatabase.getInstance().getReference(); //obtain name database
+
+        databaseReference.child(ARTIST_NODE).addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                artistNames.clear();
+                artists.clear();
+                if (dataSnapshot.exists()){
+                    for (DataSnapshot snapshot:dataSnapshot.getChildren()) {
+                        Artist artist = snapshot.getValue(Artist.class);
+                        Log.w(TAG, "Artist Name: " + artist.getName());
+                        artistNames.add(artist.getName());
+                        artists.add(artist);
+                    }
+                }
+                arrayAdapter.notifyDataSetChanged();
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+
+        lstArtist.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
+            @Override
+            public boolean onItemLongClick(AdapterView<?> parent, View view, int position, long id) {
+                String idArtist = artists.get(position).getId();
+                artists.remove(position);
+                artistNames.remove(position);
+                databaseReference.child(ARTIST_NODE).child(idArtist).removeValue();
+                return true;
+            }
+        });
+    }
+
+    public void createArtist(View view){
+        Artist artist = new Artist(databaseReference.push().getKey(), "Garbage", "Rock");
+        databaseReference.child(ARTIST_NODE).child(artist.getId()).setValue(artist);
+    }
+}
